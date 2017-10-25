2a30de51ff1cf5fcf8eb7d6bbb05774f64958ab3
Nick Stalter <nickstalter@gmail.com>
2017-10-24 21:04:08 -0400

    shits working

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <!-- <b-form @submit="feedMe"> -->
-      <b-form>
+    <b-form>
       <b-form-group
         id="locationGroup"
         label="Location"
@@ -35,13 +34,11 @@
           placeholder="Mexican"
           required></b-form-input>
         </b-form-group>
-      <!-- <b-button type="submit" variant="primary" @click="feedMe">Submit</b-button> -->
-      <b-button variant="primary" @click="feedMe">Submit</b-button>
+      <b-button type="submit" @click.prevent="submit" variant="primary">Submit</b-button>
     </b-form>
   </div>
 </template>
   <script>
-    const apiKey = 'AIzaSyDcr5TVxlv4QivDB2rd-pe3LzgpTQ6YwnE'
     export default {
 
       name: 'Index',
@@ -56,22 +53,14 @@
         }
       },
       methods: {
-        feedMe () {
-          const keywords = encodeURIComponent(this.form.keywords)
-          const location = this.form.location.replace(/ /g, '+')
-          const meters = this.form.radius * 1509
-          this.$http.get(`https://maps.googleapis.com/maps/api/geocode/json?key=${apiKey}&address=${location}`).then(response => {
-            const geolocation = `${response.body.results[0].geometry.location.lat},${response.body.results[0].geometry.location.lng}`
-            console.log('next request', `https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apiKey}&location=${geolocation}&radius=${meters}&type=restaurant&keyword=${keywords}&opennow=true`)
-            // and into another fucking promise \ 0 / praise javascript
-            this.$http.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apiKey}&location=${geolocation}&radius=${meters}&type=restaurant&keyword=${keywords}&opennow=true`).then(resp => {
-              console.log('resp.body', resp.body)
-              // this.$router.push('specials', resp)
-            }, error => {
-              console.log(error)
-            })
-          }, error => {
-            console.log(error)
+        submit () {
+          this.$router.push({
+            path: 'specials',
+            query: {
+              location: this.form.location.replace(/ /g, '+'),
+              keywords: encodeURIComponent(this.form.keywords),
+              radius: this.form.radius
+            }
           })
         }
       }
--- src/components/Specials.vue
+++ src/components/Specials.vue
@@ -1,18 +1,66 @@
 <template>
   <div>
-    <h1>Test</h1>
+    <div class="loading" v-if="loading">
+      Loading...
+    </div>
+    <div v-if="restaurants">
+      <h1>Current shit near you: </h1>
+      <div v-for="restaurant in restaurants">
+          <b-card :title="restaurant.name"
+                  :sub-title="restaurant.vicinity">
+              <p class="card-text">
+                  <div v-for="type in restaurant.types" style="display: block;">
+                    {{type}}
+                  </div>
+              </p>
+              <a :href="`https://www.google.com/maps/place/${restaurant.vicinity}`" target="_blank" 
+                 class="card-link">Directions</a>
+              <b-link href="#"
+                      class="card-link">Another link</b-link>
+          </b-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const apiKey = 'AIzaSyDcr5TVxlv4QivDB2rd-pe3LzgpTQ6YwnE'
 export default {
 
   name: 'Specials',
-
   data () {
     return {
-
+      loading: false,
+      restaurants: null
     }
+  },
+  methods: {
+    feedMe () {
+      this.loading = true
+      const keywords = this.$route.query.keywords
+      const location = this.$route.query.location
+      const meters = this.$route.query.radius * 1509
+      this.$http.get(`https://maps.googleapis.com/maps/api/geocode/json?key=${apiKey}&address=${location}`).then(response => {
+        const geolocation = `${response.body.results[0].geometry.location.lat},${response.body.results[0].geometry.location.lng}`
+        // and into another fucking promise \ 0 / praise javascript
+        this.$http.get(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=${apiKey}&location=${geolocation}&radius=${meters}&type=restaurant&keyword=${keywords}&opennow=true`).then(resp => {
+          console.log('resp.body', resp.body)
+          this.loading = false
+          this.restaurants = resp.body.results
+        }, error => {
+          console.log(error)
+        })
+      }, error => {
+        console.log(error)
+      })
+    }
+  },
+  created () {
+    this.feedMe()
+  },
+  watch: {
+    // call again the method if the route changes
+    '$route': 'feedMe'
   }
 }
 </script>
