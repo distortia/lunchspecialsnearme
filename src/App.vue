5c9c6e66fb2b5c089a391c743e82ad1efc4f8426
Nick Stalter <nickstalter@gmail.com>
2017-11-19 19:43:51 -0500

    mobile nav

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
   <div id="app">
-    <b-navbar type="light" variant="light" sticky @submit.prevent="search">
+    <b-navbar toggleable="md" type="light" variant="light" sticky @submit.prevent="search">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
       <b-navbar-brand href="/">LSNM</b-navbar-brand>
-      <b-nav-form>
-        <b-form-input
-          id="radius"
-          type="number"
-          v-model="form.radius"
-          placeholder="Radius(miles)"
-          size="sm"
-          class="mr-sm-2"
-          required></b-form-input>
-        <b-form-input 
-          id="keywords"
-          type="text"
-          v-model="form.keywords"
-          placeholder="Mexican"
-          class="mr-sm-2"
-          size="sm"
-          required></b-form-input>
-        <b-button size="sm" type="submit" variant="outline-success">Search</b-button>
-      </b-nav-form>
+      <b-collapse is-nav id="nav-collapse">
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              id="radius"
+              type="number"
+              v-model="form.radius"
+              placeholder="Radius(miles)"
+              size="sm"
+              class="mr-sm-2"
+              required></b-form-input>
+            <b-form-input 
+              id="keywords"
+              type="text"
+              v-model="form.keywords"
+              placeholder="Mexican"
+              class="mr-sm-2"
+              size="sm"
+              required></b-form-input>
+            <b-button size="sm" type="submit" variant="outline-success">Search</b-button>
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
     </b-navbar>
     <router-view></router-view>
   </div>
@@ -73,6 +78,9 @@ body {
   margin: -50px 0px 0px -50px;
 }
 @media(max-width: 767px) {
+  nav input {
+    width: 100% !important;
+  }
   nav .btn {
     width: 100%;
   } 
