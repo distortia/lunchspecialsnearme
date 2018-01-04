794f6ad7e1603e74d33adec3683934b97afa7c64
Nick Stalter <nickstalter@gmail.com>
2018-01-03 22:27:56 -0500

    lets pull from the github build instead of the old for googlemaps repo

--- server/mix.exs
+++ server/mix.exs
@@ -41,7 +41,7 @@ defmodule Lsnm.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:google_maps, "~> 0.8"},
+      {:google_maps, git: "https://github.com/sntran/ex_maps.git"},
       {:poison, "~> 3.1"},
       {:bamboo, "~> 0.8"},
       {:cors_plug, "~> 1.2"}
