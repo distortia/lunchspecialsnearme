33a8bd28a73a1546395f6d780b21c243050c3c2f
Nick Stalter <nickstalter@gmail.com>
2018-01-15 22:13:30 -0500

    created login component

---
+++ client/src/components/Login.vue
@@ -0,0 +1,65 @@
+<template>
+  <b-container fluid>
+    <b-row align-v="center" align-h="center">
+      <b-col md="4">
+        <b-card header="Login"
+              border-variant="primary"
+              header-bg-variant="primary"
+              header-text-variant="white"
+              class="login-card">
+          <p class="card-text">
+            <b-form @submit="login">
+              <b-form-group id="emailGroup"
+                      label="Email address:"
+                      label-for="email">
+                <b-form-input id="email"
+                        type="email"
+                        v-model="form.email"
+                        required
+                        placeholder="Enter email">
+                </b-form-input>
+              </b-form-group>
+              <b-form-group id="passwordGroup"
+                      label="Password:"
+                      label-for="password">
+                <b-form-input id="password"
+                        type="password"
+                        v-model="form.password"
+                        required
+                        placeholder="Enter Password">
+                </b-form-input>
+              </b-form-group>
+              <b-button type="submit" variant="primary" block>Login</b-button>
+            </b-form>
+          </p>
+          <router-link to="/register" class="card-link">Or Register</router-link>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+
+  name: 'Login',
+
+  data () {
+    return {
+      form: {
+        email: null,
+        password: null
+      }
+    }
+  },
+  methods: {
+    login() {
+      console.log('Fill me out')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/sass/login';
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import Specials from '@/components/Specials'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     { path: '/', name: 'index', component: Index },
-    { path: '/specials', name: 'Specials', component: Specials }
+    { path: '/specials', name: 'Specials', component: Specials },
+    { path: '/login', name: 'Login', component: Login }
   ]
 })
