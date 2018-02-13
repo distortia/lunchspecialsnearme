38edef4ff973c5bca578c8fb074b96de8ce1a035
shibuyamizou <nickstalter@gmail.com>
2018-02-12 22:47:43 -0500

    updated api server port

--- server/config/dev.exs
+++ server/config/dev.exs
@@ -7,7 +7,7 @@ use Mix.Config
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :lsnm, LsnmWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("API_SERVER_PORT") || "4000"],
   debug_errors: true,
   code_reloader: true,
   check_origin: false
